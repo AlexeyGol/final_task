a5980abba2bcbe27e9bc452a5e66dbe0ac7a9360
Oleksii H <oleksii.golik@gmail.com>
2022-06-29 12:35:25 +0300

    modified Jenkinsfile, added app

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "Jenkins_public_ip" {
    value = module.Jenkins_master.instance.public_ip
 }
 
-output "Jenkins_URL" {
-  value = "http://${module.Jenkins_master.instance.public_ip}:8080"
+output "add_Jenkins_URL_to_actual_jenkins_location" {
+  value = "http://${module.Jenkins_master.instance.public_ip}:8080/configure"
+}
+
+output "add_webhook_to_github" {
+  value = "https://github.com/AlexeyGol/final_task/settings/hooks http://${module.Jenkins_master.instance.public_ip}:8080/github-webhook"
 }
