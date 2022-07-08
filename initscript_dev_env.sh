b24eeac43e9299e0656ebd12f3c97fdaa6fe099f
Oleksii H <oleksii.golik@gmail.com>
2022-07-08 15:07:03 +0300

    jenkinsfile - solve Docker problem

--- initscript_dev_env.sh
+++ initscript_dev_env.sh
@@ -5,6 +5,6 @@ export DOCKERHUB_USR=$2
 export DOCKERHUB_PWD=$3
 
 whoami
-curl http://checkip.amazonaws.com
-echo $DOCKERHUB_PWD | docker login -u $DOCKERHUB_USR --password-stdin
-docker run -p 8080:8080 -d $IMAGE && echo "COntainer was started"
+# curl http://checkip.amazonaws.com
+# echo $DOCKERHUB_PWD | docker login -u $DOCKERHUB_USR --password-stdin
+# docker run -p 8080:8080 -d $IMAGE && echo "COntainer was started"
