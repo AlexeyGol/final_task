2bb3383e220da2755ee2fdacb62fd62ccf959c85
Oleksii H <oleksii.golik@gmail.com>
2022-07-09 20:39:07 +0300

    Jenkinsfile - solving ssh problem

--- dev_script.sh
+++ dev_script.sh
@@ -4,5 +4,5 @@ export DOCKER_USER=$1
 export DOCKER_PASSWORD=$2
 
 uptime
-echo $DOCKER_PASSWORD | docker login -u="$DOCKER_USER" --password-stdin
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
 
