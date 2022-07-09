ebdf4fe061fcad15eb1acc09e9357bb9c6205e0f
Oleksii H <oleksii.golik@gmail.com>
2022-07-09 21:06:55 +0300

    Jenkinsfile - solving ssh problem

--- dev_script.sh
+++ dev_script.sh
@@ -7,6 +7,6 @@ export DOCKER_IMAGE=$3
 uptime
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
 docker pull $DOCKER_IMAGE
-docker rm -f $(docker ps -a -q)
+docker rm -f $(docker ps -aq)
 docker run -p 8080:8080 -d $DOCKER_IMAGE
-
+docker rmi $(docker images -aq)
